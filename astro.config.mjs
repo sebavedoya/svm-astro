43a2d1ff32d750ff4109e6d28ae536354566cab7
Sebastián Vedoya <sebastianvedoya@MacBook-Air-de-Sebastian.local>
2025-11-03 00:20:43 -0300

    Implement new site architecture with dedicated pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://svm-astro.vercel.app',
-  integrations: [tailwind(), react(), sitemap(), mdx()],
+  integrations: [tailwind(), react(), sitemap()],
 });
