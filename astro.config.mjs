09370a5e7d6e45398e3932c97c5668f2a9666358
Sebastián Vedoya <sebastianvedoya@MacBook-Air-de-Sebastian.local>
2025-11-03 00:07:33 -0300

    Retime site architecture with MDX pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://svm-astro.vercel.app',
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [tailwind(), react(), sitemap(), mdx()],
 });
